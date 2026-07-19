43af6482c7ce33d8c6394661937155c490b7f3f5
“imprjoseph” <chengi.joseph@gmail.com>
2026-07-19 17:14:56 +0800

    Fix website tab titles

--- assets/conference-config.js
+++ assets/conference-config.js
@@ -1,5 +1,5 @@
 /**
- * 2026 跨境數位治理高峰論壇
+ * 2026科技主權高峰論壇
  *
  * 新網站 Repository 建立後，將本檔複製為 assets/conference-config.js。
  * apiUrl 已連接此會議專用的 Google Apps Script 部署。
@@ -8,7 +8,7 @@ window.CONFERENCE_CONFIG = Object.freeze({
     siteId: 'cross-border-digital-governance-2026',
     apiUrl: 'https://script.google.com/macros/s/AKfycbwpbyEaqn-cIm13uRamNFgGY2dkmQWlUSNL79BVjykm6mqxnbEK_2zZQPFbB8TD-V9d/exec',
     googleSheetId: '1r82HCNk51leDTqTptNPis1cnvP9TMx-54i7ZRIyPGLY',
-    siteTitle: '2026 跨境數位治理高峰論壇',
-    adminTitle: '2026 跨境數位治理高峰論壇｜管理後台',
+    siteTitle: '2026科技主權高峰論壇',
+    adminTitle: '2026科技主權高峰論壇｜管理後台',
     customDomain: ''
 });
